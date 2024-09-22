81d20884d3a0c2779c334b97b0cfc312e05297d1
lesmana185 <lesmanaariel08@gmail.com>
2024-09-22 14:14:18 +0700

    menambahkan fitur search dan sedikit merubah bagian menu

--- js/script.js
+++ js/script.js
@@ -1,15 +1,39 @@
-// Toggle Class Active
+// Toggle Class Active untuk Hamburger Menu
 const navbarNav = document.querySelector('.navbar-nav');
 
 // Ketika hamburger menu di Klik
-document.querySelector('#hamburger-menu').onclick = () => {
+document.querySelector('#hamburger-menu').onclick = (e) => {
     navbarNav.classList.toggle('active');
+    e.preventDefault();
 };
 
-// ketika di klik diluar sidebar menghilangkan nav
-const hamburger = document.querySelector('#hamburger-menu');
+// Toggle Class Active Untuk Search Form
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+
+// ketika search di klik
+document.querySelector('#search-button').onclick = (e) => {
+    searchForm.classList.toggle('active');
+    searchBox.focus();
+    e.preventDefault();
+}
+
+// ketika di klik diluar Element
+const hm = document.querySelector('#hamburger-menu');
+const sb = document.querySelector('#search-button');
+
 document.addEventListener('click', function(e) {
-    if( !hamburger.contains(e.target) && !navbarNav.contains(e.target) ) {
+    if( !hm.contains(e.target) && !navbarNav.contains(e.target) ) {
         navbarNav.classList.remove('active');
     }
+
+    if( !sb.contains(e.target) && !searchForm.contains(e.target) ) {
+        searchForm.classList.remove('active');
+    }
 });
+
+
+
+
+
+
