99c38ba9d4b937cd0f4be32ca874c18264ce90cd
lesmana185 <lesmanaariel08@gmail.com>
2024-09-22 07:06:23 +0700

    membuat website kopi

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Toggle Class Active
+const navbarNav = document.querySelector('.navbar-nav');
+
+// Ketika hamburger menu di Klik
+document.querySelector('#hamburger-menu').onclick = () => {
+    navbarNav.classList.toggle('active');
+};
+
+// ketika di klik diluar sidebar menghilangkan nav
+const hamburger = document.querySelector('#hamburger-menu');
+document.addEventListener('click', function(e) {
+    if( !hamburger.contains(e.target) && !navbarNav.contains(e.target) ) {
+        navbarNav.classList.remove('active');
+    }
+});
