60417d7f03dca94cb3b9b1b8bd56cb7ef86fdc0a
lesmana185 <lesmanaariel08@gmail.com>
2024-09-23 08:24:02 +0700

    menjalankan fitur search, menjalankan shopping cart dan menambahkan fitur modal detail

--- js/script.js
+++ js/script.js
@@ -18,9 +18,19 @@ document.querySelector('#search-button').onclick = (e) => {
     e.preventDefault();
 }
 
+// Toggle Class Active Untuk Shopping Cart
+const shoppingCart = document.querySelector('.shopping-cart');
+
+// Ketika shopping cart di klik
+document.querySelector('#shopping-cart-button').onclick = (e) => {
+    shoppingCart.classList.toggle('active');
+    e.preventDefault();
+}
+
 // ketika di klik diluar Element
 const hm = document.querySelector('#hamburger-menu');
 const sb = document.querySelector('#search-button');
+const scb = document.querySelector('#shopping-cart-button');
 
 document.addEventListener('click', function(e) {
     if( !hm.contains(e.target) && !navbarNav.contains(e.target) ) {
@@ -30,9 +40,41 @@ document.addEventListener('click', function(e) {
     if( !sb.contains(e.target) && !searchForm.contains(e.target) ) {
         searchForm.classList.remove('active');
     }
+    if( !scb.contains(e.target) && !shoppingCart.contains(e.target) ) {
+        shoppingCart.classList.remove('active');
+    }
 });
 
+// Modal Box
+const itemDetailModal = document.querySelector('#item-detail-modal');
+const itemDetailButton = document.querySelectorAll('.item-detail-button');
 
+// Ketika tombol mata di klik
+itemDetailButton.forEach ((btn) => {
+    btn.onclick = (e) => {
+        itemDetailModal.style.display = 'flex';
+        e.preventDefault();
+    }
+
+}) 
+
+
+
+
+// Ketika tombol close di klik
+
+document.querySelector('.modal-container .close-icon').onclick = (e) => {
+    itemDetailModal.style.display = 'none';
+    e.preventDefault();
+}
+
+
+// Ketika di klik di luar element
+window.onclick = (e) => {
+    if ( e.target === itemDetailModal ) {
+        itemDetailModal.style.display = 'none';
+    }
+}
 
 
 
